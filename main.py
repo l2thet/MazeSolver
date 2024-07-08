e1bd855788587b56dec2daac5a1a9ad43aee31df
Captum <dan.w00z@gmail.com>
2024-07-08 18:23:20 -0400

    Adding and using the Maze class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.cell import Cell
 from src.line import Line
+from src.maze import Maze
 from src.point import Point
 from src.window import Window
 
@@ -10,13 +11,14 @@ def main():
     #line = Line(Point(0, 0), Point(1024, 768), "green")
     #window.draw_line(line)
     
-    cell_one = Cell(Point(100, 100), Point(200, 200), True, True, True, True)
-    cell_two = Cell(Point(200, 100), Point(300, 200), True, True, True, True)
-    window.draw_cell(cell_one, "green")
-    window.draw_cell(cell_two, "blue")
+    # cell_one = Cell(Point(100, 100), Point(200, 200), True, True, True, True)
+    # cell_two = Cell(Point(200, 100), Point(500, 200), True, True, True, True)
+    # window.draw_cell(cell_one, "green")
+    # window.draw_cell(cell_two, "blue")
     
-    cell_one.draw_move(window.canvas, cell_two, True)
+    # cell_one.draw_move(window.canvas, cell_two)
     
+    maze = Maze(0, 0, 10, 10, 50, 50, window)
     
     window.wait_for_close()
         
---
+++ src/maze.py
@@ -0,0 +1,48 @@
+from time import sleep
+from src.cell import Cell
+from src.point import Point
+
+
+class Maze:
+    def __init__(
+      self,
+      x1,
+      y1,
+      num_rows,
+      num_cols,
+      cell_size_x,
+      cell_size_y,
+      win  
+    ):
+        self.x1 = x1
+        self.y1 = y1
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size_x = cell_size_x
+        self.cell_size_y = cell_size_y
+        self.win = win
+        self._cells = []
+        self._create_cells()
+        
+        
+    def _create_cells(self):
+        self._cells = []
+        for i in range(self.num_rows):
+            row = []
+            for j in range(self.num_cols):
+                top_left = Point(self.x1 + j * self.cell_size_x, self.y1 + i * self.cell_size_y)
+                bottom_right = Point(self.x1 + (j + 1) * self.cell_size_x, self.y1 + (i + 1) * self.cell_size_y)
+                cell = Cell(top_left, bottom_right, True, True, True, True)
+                row.append(cell)
+                self._cells.append(cell)
+                self._draw_cell(cell, "white")
+            self._cells.append(row)
+            
+    
+    def _draw_cell(self, cell, color):
+        cell.draw(self.win.canvas, color)
+        self._animate()
+        
+    def _animate(self):
+        self.win.redraw()
+        sleep(0.05)
