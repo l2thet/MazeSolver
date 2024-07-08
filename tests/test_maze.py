ad2015265a77a0bb7f0d8956af36d40709653fb2
Captum <dan.w00z@gmail.com>
2024-07-08 19:03:17 -0400

    setting up files needed for UT auto detection in VSCode

---
+++ tests/test_maze.py
@@ -0,0 +1,11 @@
+import unittest
+
+from src.maze import Maze
+
+class TestMaze(unittest.TestCase):
+    def test(self):
+        self.assertEqual(1, 1)
+        
+
+if __name__ == "__main__":
+    unittest.main()
